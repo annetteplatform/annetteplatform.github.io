f9c3386ff028d888671b739cd7daaa50dacbc74a
Valery Lobachev <valery.lobachev@gmail.com>
2020-01-16 14:03:32 +0300

    Move to docs

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,10 @@
 # abort on errors
 set -e
 # build
-vuepress build 
+vuepress build docs
 
 # navigate into the build output directory
-cd .vuepress/dist
+cd docs/.vuepress/dist
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
@@ -15,8 +15,7 @@ git init
 git add -A
 git commit -m 'deploy'
 
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/annetteplatform/annetteplatform.github.io.git master:gh-pages
+# if you are deploying to https://<USERNAME>.github.io
+git push -f https://github.com/annetteplatform/annetteplatform.github.io.git master
 
 cd -
