11e8d1945e7c90d09c1a4f36543d7a7187d02af0
Valery Lobachev <valery.lobachev@gmail.com>
2021-12-29 19:58:20 +0300

    Updated versions

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,18 +1,28 @@
-module.exports = ctx => ({
-    head: [
-        ["link", {rel: "icon", href: "/favicon.png"}],
-        ['script', {
-            async: true,
-            src: 'https://www.googletagmanager.com/gtag/js?id=G-NH2SEQB56T'
-        }],
-        ['script', {}, `
+module.exports = (ctx) => ({
+  head: [
+    ["link", { rel: "icon", href: "/favicon.png" }],
+    [
+      "script",
+      {
+        async: true,
+        src: "https://www.googletagmanager.com/gtag/js?id=G-NH2SEQB56T",
+      },
+    ],
+    [
+      "script",
+      {},
+      `
           window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', 'G-NH2SEQB56T');
-      `],
-        ['script', {}, `
+      `,
+    ],
+    [
+      "script",
+      {},
+      `
            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
    m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
@@ -22,93 +32,92 @@ module.exports = ctx => ({
         trackLinks:true,
         accurateTrackBounce:true
    });
-    `]
+    `,
     ],
+  ],
+  locales: {
+    "/": {
+      lang: "en-US",
+      title: "Annette Platform",
+      description:
+        "Platform to build distributed, scalable, enterprise-wide business applications",
+    },
+    // "/ru/": {
+    //   lang: "ru-RU",
+    //   title: "Платформа Annette",
+    //   description: "Платформа для создания бизнес приложений"
+    // }
+  },
+
+  //theme: '@vuepress/vue',
+  themeConfig: {
+    smoothScroll: true,
     locales: {
-        "/": {
-            lang: "en-US",
-            title: "Annette Platform",
-            description: "Platform to build distributed, scalable, enterprise-wide business applications"
+      "/": {
+        label: "English",
+        selectText: "Languages",
+        ariaLabel: "Select language",
+        lastUpdated: "Last Updated",
+        nav: require("./nav/en"),
+        sidebar: {
+          "/get-started/": getGetStartedSidebar("Get Started", "Advanced"),
+          "/case-studies/": getCaseStudiesSidebar("Case studies"),
+          "/guide/": getGuideSidebar("Guide"),
         },
-        // "/ru/": {
-        //   lang: "ru-RU",
-        //   title: "Платформа Annette",
-        //   description: "Платформа для создания бизнес приложений"
-        // }
+      },
+      // "/ru/": {
+      //   label: "Русский",
+      //   selectText: "Языки",
+      //   ariaLabel: "Выберите язык",
+      //   lastUpdated: "Последнее обновление",
+      //   nav: require("./nav/ru"),
+      //   sidebar: {}
+      // }
     },
+  },
 
-    //theme: '@vuepress/vue',
-    themeConfig: {
-        smoothScroll: true,
-        locales: {
-            "/": {
-                label: "English",
-                selectText: "Languages",
-                ariaLabel: "Select language",
-                lastUpdated: "Last Updated",
-                nav: require("./nav/en"),
-                sidebar: {
-                    "/get-started/": getGetStartedSidebar("Get Started", "Advanced"),
-                    "/case-studies/": getCaseStudiesSidebar("Case studies"),
-                    "/guide/": getGuideSidebar("Guide"),
-                }
-            },
-            // "/ru/": {
-            //   label: "Русский",
-            //   selectText: "Языки",
-            //   ariaLabel: "Выберите язык",
-            //   lastUpdated: "Последнее обновление",
-            //   nav: require("./nav/ru"),
-            //   sidebar: {}
-            // }
-        }
+  plugins: {
+    "@vuepress/medium-zoom": {
+      selector: "img",
+      delay: 1000,
+      options: {
+        margin: 24,
+        background: "#FFFFFF",
+        scrollOffset: 0,
+      },
     },
-
-    plugins: [
-        [
-            'vuepress-plugin-medium-zoom',
-            {
-                selector: 'img',
-                delay: 1000,
-                options: {
-                    margin: 24,
-                    background: '#FFFFFF',
-                    scrollOffset: 0,
-                },
-            },
-        ]
-    ],
+  },
 });
 
 function getGuideSidebar(groupA) {
-    return [""];
+  return [""];
 }
 
 function getGetStartedSidebar(groupA, groupB) {
-    return [
-        {
-            title: groupA,
-            collapsable: false,
-            sidebarDepth: 2,
-            children: ["", "features", "install"]
-        },
-        {
-            title: groupB,
-            collapsable: false,
-            sidebarDepth: 2,
-            children: ["build"]
-        }
-    ];
+  return [
+    {
+      title: groupA,
+      collapsable: false,
+      sidebarDepth: 2,
+      children: ["", "features", "install"],
+    },
+    {
+      title: groupB,
+      collapsable: false,
+      sidebarDepth: 2,
+      children: ["build"],
+    },
+  ];
 }
 
 function getCaseStudiesSidebar(groupA) {
-    return [
-        {
-            title: groupA,
-            collapsable: false,
-            sidebarDepth: 2,
-            children: [""]
-            // children: ["", "tele2"/*, "emercom", "mimc", "cpm", "work-safety", "emlife"*/]
-        }
-    ]
+  return [
+    {
+      title: groupA,
+      collapsable: false,
+      sidebarDepth: 2,
+      children: [""],
+      // children: ["", "tele2"/*, "emercom", "mimc", "cpm", "work-safety", "emlife"*/]
+    },
+  ];
 }
