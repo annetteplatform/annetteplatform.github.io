b48116d97edacae84ec3dee98a1e65a112f9c240
Valery Lobachev <valery.lobachev@gmail.com>
2022-06-20 13:10:52 +0300

    Added authorization API

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -101,7 +101,7 @@ module.exports = (ctx) => ({
                     // title: 'API',
                     collapsable: false,
                     sidebarDepth: 4,
-                    children: ['', 'application'],
+                    children: ['', 'application', 'authorization'],
                 },
             ],
             '/guide/': [{
